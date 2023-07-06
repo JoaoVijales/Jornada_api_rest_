0956c0ed803a03f2b80ded36930175a64887eb8c
Schneider <vijales2000@gmail.com>
2023-07-06 11:38:56 -0300

    criando rota para depoimentos

--- setup/urls.py
+++ setup/urls.py
@@ -1,10 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from jornada.views import DepoimentosViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
+
+router.register('depoimentos', DepoimentosViewSet, basename='Depoimentos')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',    include(router.urls))
