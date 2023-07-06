d2c4714d94ba8f18f231f173c3e7968cb8edaf4c
Schneider <vijales2000@gmail.com>
2023-07-06 12:34:56 -0300

    criando rota que retorna um depoimento aleatorio

--- jornada/views.py
+++ jornada/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from jornada.models import Depoimentos
 from jornada.serializers import DepoimentosSerializer
+from random import randint
 
 
 class DepoimentosViewSet(viewsets.ModelViewSet):
+    # exibindo depoimentos #
     queryset = Depoimentos.objects.all()
     serializer_class = DepoimentosSerializer
 
+class DepoimentosHomeViewSet(viewsets.ModelViewSet):
+    # exibindo um depoimento aleatorio #
+    total_objects = Depoimentos.objects.count() -1
+    id = randint(0, total_objects)
+    
+    queryset = Depoimentos.objects.filter(id=id)
+    serializer_class = DepoimentosSerializer
