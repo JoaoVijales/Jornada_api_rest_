2cc13376a1407cce0ab52aaf3c23ee2ea9157fe3
Schneider <vijales2000@gmail.com>
2023-07-06 11:49:00 -0300

    fazendo migrações para o banco de dados

--- jornada/models.py
+++ jornada/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Depoimentos(models.Model):
-    nome = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    mensagem = models.TextField()
+    nome = models.CharField(max_length=50, blank=False, null=False)
+    foto = models.ImageField(upload_to='fotos', blank=True,)
+    depoimento = models.TextField(max_length=500, blank=False, null=False)
