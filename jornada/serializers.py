0b4cd690c5b33f3857393303a2dc3571732f09ae
Schneider <vijales2000@gmail.com>
2023-07-21 17:33:02 -0300

    criando serializer de destinos

--- jornada/serializers.py
+++ jornada/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
-from jornada.models import Depoimentos
+from jornada.models import Depoimentos, Destinos
 
 class DepoimentosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Depoimentos
         fields = '__all__'
 
+class DestinosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Depoimentos
+        fields = ['nome', 'foto', 'preco']
