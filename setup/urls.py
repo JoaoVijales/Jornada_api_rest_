ce09c411d7130348134ea777b7075717e5bd78b0
Schneider <vijales2000@gmail.com>
2023-07-21 17:36:36 -0300

    registrando rota de Destinos

--- setup/urls.py
+++ setup/urls.py
@@ -1,13 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from jornada.views import DepoimentosViewSet, DepoimentosHomeViewSet
+from jornada.views import DepoimentosViewSet, DepoimentosHomeViewSet, DestinosViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 
 router.register('depoimentos', DepoimentosViewSet, basename='Depoimentos')
 router.register('depoimentos-home', DepoimentosHomeViewSet, basename='DepoimentosHome')
+router.register('destinos', DestinosViewSet, basename='Destinos')
 
 
 urlpatterns = [
